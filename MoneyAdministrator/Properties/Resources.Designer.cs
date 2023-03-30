d6ee72e45772507021ea4bc0b872375b86d9d480
Lucas Ceratto <72511202+lucerattow@users.noreply.github.com>
2023-03-29 21:37:23 -0300

    Añado el icono de "Salir"

--- MoneyAdministrator/Properties/Resources.Designer.cs
+++ MoneyAdministrator/Properties/Resources.Designer.cs
@@ -203,6 +203,16 @@ namespace MoneyAdministrator.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap exit_shadow {
+            get {
+                object obj = ResourceManager.GetObject("exit_shadow", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap eye {
             get {
                 object obj = ResourceManager.GetObject("eye", resourceCulture);
