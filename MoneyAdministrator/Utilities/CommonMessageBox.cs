eb3666064fd93a48be6caad3b2e4981f47776b4c
Lucas Ceratto <72511202+lucerattow@users.noreply.github.com>
2023-03-29 21:36:03 -0300

    Añado una clase para generar mensajes de forma rapida

---
+++ MoneyAdministrator/Utilities/CommonMessageBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyAdministrator.Utilities
+{
+    public class CommonMessageBox
+    {
+        public static DialogResult errorMessageShow(string message, MessageBoxButtons buttons)
+        {
+            return MessageBox.Show(message, $"{ConfigurationManager.AppSettings["AppTitle"]} : Error", buttons, MessageBoxIcon.Error);
+        }
+    }
+}
